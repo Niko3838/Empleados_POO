42a02796c0701014225852359a377f9528eb6cb8
Nixon Benavides <lubierb383@gmail.com>
2023-06-29 02:52:19 -0500

    Agregacion de Estilo y Diseño @nixon

--- js/funciones.js
+++ js/funciones.js
@@ -1,51 +1,45 @@
 function calcularEdad() {
     var fechaNacimiento = document.getElementById("nacimiento").value;
     var fechaActual = new Date();
-  
     var partesFechaNacimiento = fechaNacimiento.split("-");
     var anioNacimiento = parseInt(partesFechaNacimiento[0]);
     var mesNacimiento = parseInt(partesFechaNacimiento[1]);
     var diaNacimiento = parseInt(partesFechaNacimiento[2]);
   
-    var edad = fechaActual.getFullYear() - anioNacimiento;
-    
-    if (fechaNacimiento == 'Invalid Date') {
-        alert("Ingrese la Fecha de Nacimiento");
+    if (fechaNacimiento === "") {
+        alert("Por favor, ingresa la fecha de nacimiento.");
         return;
-    }else{
-        if (
-            fechaActual.getMonth() < mesNacimiento ||
-            (fechaActual.getMonth() === mesNacimiento && fechaActual.getDate() < diaNacimiento)
-          ) {
-            edad--;
-          }
     }
 
+    var edad = fechaActual.getFullYear() - anioNacimiento;
+    
+    if (fechaActual.getMonth() < mesNacimiento || (fechaActual.getMonth() === mesNacimiento && fechaActual.getDate() < diaNacimiento)) {
+        edad--;
+    }
     document.getElementById("mostraredad").value = edad;
-  }
-
-  function calcularAntiguedad() {
+}
+//Calcular la antiguedad deseada
+function calcularAntiguedad() {
     var fechaIngreso = document.getElementById("ingreso").value;
     var fechaActual = new Date();
-  
     var partesIngreso = fechaIngreso.split("-");
     var anioIngreso = parseInt(partesIngreso[0]);
     var mesIngreso = parseInt(partesIngreso[1]);
     var diaIngreso = parseInt(partesIngreso[2]);
-  
+
+    if (fechaIngreso === "") {
+        alert("Por favor, ingresa la fecha de Ingreso.");
+        return;
+    }
     var antiguedad = fechaActual.getFullYear() - anioIngreso;
-  
-    if (
-      fechaActual.getMonth() < mesIngreso ||
-      (fechaActual.getMonth() === mesIngreso && fechaActual.getDate() < diaIngreso)
-    ) {
+    if (fechaActual.getMonth() < mesIngreso || (fechaActual.getMonth() === mesIngreso && fechaActual.getDate() < diaIngreso)) {
       antiguedad--;
     }
     document.getElementById("mostrarantiguedad").value = antiguedad;
-  }
+}
 
 
-  function calcularPrestaciones() {
+function calcularPrestaciones() {
     var salario = parseFloat(document.getElementById("salario").value);
     var fechaIngreso = document.getElementById("ingreso").value;
     var fechaActual = new Date();
@@ -58,24 +52,33 @@ function calcularEdad() {
 
     var prestaciones = (anti*salario)/12;
     document.getElementById("mostrarprestaciones").value = prestaciones.toFixed(2);
+}
 
-  }
-  
-  function calcularAntiguedad2(fechaActual, fechaIngreso) {
-      
+//Calculamos la antiguedad de la persona para la funcion de Prestaciones
+function calcularAntiguedad2(fechaActual, fechaIngreso) {
     var partesIngreso = fechaIngreso.split("-");
     var anioIngreso = parseInt(partesIngreso[0]);
     var mesIngreso = parseInt(partesIngreso[1]);
     var diaIngreso = parseInt(partesIngreso[2]);
-
     var antiguedad = fechaActual.getFullYear() - anioIngreso;
 
-    if (
-    fechaActual.getMonth() < mesIngreso ||
-    (fechaActual.getMonth() === mesIngreso && fechaActual.getDate() < diaIngreso)
-    ) {
+    if (fechaActual.getMonth() < mesIngreso || (fechaActual.getMonth() === mesIngreso && fechaActual.getDate() < diaIngreso)) {
     antiguedad--;
     }
-
     return antiguedad;
-  }
+}
+
+function modificarSalario() {
+    let cant = prompt("Ingrese el Salario");
+    document.getElementById('salario').value = cant;
+}
+
+
+
+
+window.onload = function() {
+    var today = new Date().toISOString().split('T')[0];
+    document.getElementById("ingreso").setAttribute("max", today);
+    document.getElementById("ingreso").addEventListener("input", validarFecha);
+}
+
